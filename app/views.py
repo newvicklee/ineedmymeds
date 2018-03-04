5258ccc167596becaf7a04a1b650eedbd0a8752d
Pablo Duboue <pablo.duboue@gmail.com>
2018-03-03 23:01:24 -0500

    Search functionality almost fleshed out

--- app/views.py
+++ app/views.py
@@ -1,44 +1,49 @@
+import sqlite3
+import re
+import datetime
+import json
+
 from flask import render_template, flash, abort, redirect, session, url_for, request, g, jsonify, make_response
 from app import app
 
 
+from flask import g
 
-pharmacy1 = {
-        'pharmacy-id': 12345,
-        'pharmacy-name': 'Shoppers Drug Mart #0',
-        'pharmacy-address': '123 Main St',
-        'pharmacy-phone': '604-111-1111'
-        }
+DATABASE = 'ineedmy.db'
 
-pharmacy2 = {
-        'pharmacy-id': 12345,
-        'pharmacy-name': 'Shoppers Drug Mart #0',
-        'pharmacy-address': '123 Main St',
-        'pharmacy-phone': '604-111-1111'
-        }
-"""
-search_stub = {
-    'drug-id': 5,
-    'other-drugs': [  { 'id': 2, 'name': 'other' }, {'id':3, 'name': 'yet another'} ],
-    'known': True
-}
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-"""
+def now_int():
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    now = datetime.datetime.utcnow()
+    return (now - epoch).total_seconds()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+        
 search_stub = {
     'drug-id': 5,
     'other-drugs': [  { 'id': 2, 'name': 'other' }, {'id':3, 'name': 'yet another'} ],
     'pharmacies': [
         { 
-            'pharmacy_id': 12345,
-            'pharmacy_name': 'Shoppers Drug Mart #0',
-            'pharmacy_address': '123 Main St',
-            'pharmacy_phone': '604-111-1111'
+            'id': 12345,
+            'name': 'Shoppers Drug Mart #0',
+            'address': '123 Main St',
+            'phone': '604-111-1111'
         },
         { 
-            'pharmacy_id': 12345,
-            'pharmacy_name': 'Shoppers Drug Mart #0',
-            'pharmacy_address': '123 Main St',
-            'pharmacy_phone': '604-111-1111'
+            'id': 12345,
+            'name': 'Shoppers Drug Mart #0',
+            'address': '123 Main St',
+            'phone': '604-111-1111'
         },
         ],
     'known': True
@@ -74,11 +79,91 @@ def login():
 
 @app.route('/api/v1/search', methods=['GET'])
 def search():
-    drug = request.args.get('drug')
+    drug = request.args.get('drug').lower()
     location = request.args.get('location')
-    #TODO find drug in DB
-    #TODO find locatio through web-service
-    return jsonify(search_stub)
+
+    # get DB
+    db = get_db()
+    cur = db.cursor()
+    
+    # store search
+    now = now_int()
+    cur.execute('INSERT INTO Searches VALUES(?,?)', (drug, now))
+    db.commit()
+    
+    # find drug in DB
+    cur.execute('SELECT drug_id FROM DrugNames WHERE Name = ?', (drug,))
+    exact_row = cur.fetchone()
+    found = exact_row is not None
+
+    # find variants
+    no_spaces = re.sub('\\s+', '', drug)
+    id_counts = dict()
+    for idx in range(len(no_spaces)-2):
+        trigram = no_spaces[idx:(idx+3)]
+        for drug_id in cur.execute('SELECT drug_id FROM DrugNameParts WHERE Part = ? LIMIT 100', (trigram,)):
+            id_counts[drug_id[0]] = id_counts.get(drug_id[0], 0) + 1
+
+    sorted_id_counts = sorted(list(id_counts.items()), key=lambda tup: -1 * tup[1])
+
+    result = {'known': found}
+    if found:
+        drug_id = exact_row[0]
+        result['drug-id'] = drug_id
+
+        # TODO get location using https://bcgov.github.io/ols-devkit/ale/
+        loc_lat = 49.28202
+        loc_long = -123.11875
+
+        # record request
+        cur.execute('INSERT INTO DrugRequests VALUES (?,?,?,?)', (drug_id,now,loc_lat,loc_long))
+        db.commit()
+
+        # fetch availability
+        pharmacies = list()
+        for row in cur.execute("""SELECT pharma_id, 
+(lat - ?) * (lat - ?) + (long - ?) * (long - ?) AS distance
+FROM PharmaLoc
+ORDER BY distance ASC
+LIMIT 10""", (loc_lat, loc_lat, loc_long, loc_long)):
+            pharma_id = row[0]
+            pharmacies.append({ 'id' : pharma_id })
+
+        for idx in range(len(pharmacies)):
+            pharma_id = pharmacies[idx]['id']
+            doc = json.loads(cur.execute(
+                'SELECT doc FROM PharmaDoc WHERE oid = ?', (pharma_id,)).fetchone()[0])
+            doc['id'] = pharma_id
+            pharmacies[idx] = doc
+
+            cur.execute(
+                """SELECT availability, when_reported, by_pharmacist 
+FROM Availabilities 
+WHERE pharma_id = ?
+AND drug_id = ?
+ORDER BY when_reported""", (pharma_id,drug_id))
+            row = cur.fetchone()
+            if row is None:
+                pharmacies[idx]['available'] = 'unknown'
+            else:
+                if row[0]:
+                    pharmacies[idx]['available'] = 'yes'
+                else:
+                    pharmacies[idx]['available'] = 'no'
+
+        result['pharmacies'] = pharmacies
+
+    variants = []
+    for variant_id, count in sorted_id_counts[:10]:
+        cur.execute('SELECT name FROM DrugNames WHERE drug_id = ?', (variant_id,))
+        variants.append({
+            'id': variant_id,
+            'name': cur.fetchone()[0]
+        })
+
+    result['other-drugs'] = variants
+
+    return jsonify(result)
 
 @app.route('/api/v1/drug/<int:drug_id>/at/<int:pharmacy_id>/available', methods=['GET'])
 def is_available(drug_id, pharmacy_id):
