06bbe86712d97aeb0c4276ec00cefa3eee9f8c36
Newvick Lee <nvlee15@gmail.com>
2018-04-28 16:41:41 -0700

    add react-router-dom

---
+++ frontend/components/Nav.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var NavLink = require('react-router-dom').NavLink;
+import logo from '../imgs/logo.png';
+
+function Nav () {
+  return (
+    <nav className='menu-container'>
+        <div className="menu">
+            <NavLink exact to='/' className="navbar-brand">
+                <span className="navbar-logo"><img className="logo" src={logo}/></span>
+            </NavLink>
+            <div>
+                <NavLink to='/pharmacy' className="login">Pharmacy? Log In</NavLink>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+module.exports = Nav;
---
+++ frontend/components/Pharmacy.js
@@ -0,0 +1,12 @@
+const React = require('react');
+
+
+class Pharmacy extends React.Component {
+    render() {
+        return (
+            <div>This works!</div>
+        )
+    };
+};
+
+module.exports = Pharmacy;
